d579822ccf1af2ef3c36edb09f509b04bdd18c0b
Pasha-Akito <1masterpasha@gmail.com>
2022-09-01 10:16:25 +0100

    Added Heal Button Function

--- app.js
+++ app.js
@@ -13,10 +13,13 @@ const app = Vue.createApp({
     },
     computed: {
         monsterBarStyles() {
-            return {width: this.monsterHealth + '%'}
+            return { width: this.monsterHealth + '%' }
         },
         playerBarStyles() {
-            return {width: this.playerHealth + '%'}
+            return { width: this.playerHealth + '%' }
+        },
+        mayUseSpecialAttack() {
+            return this.currentRound % 3 !== 0;
         }
     },
     methods: {
@@ -34,6 +37,14 @@ const app = Vue.createApp({
             this.currentRound++;
             const attackValue = getRandomValue(10, 25);
             this.monsterHealth -= attackValue;
+            this.attackPlayer();
+        },
+        healPlayer() {
+            this.currentRound++;
+            const healValue = getRandomValue(8, 20);
+            if (this.playerHealth + healValue > 100)
+            this.playerHealth = 100;
+            else this.playerHealth += healValue;
             this.attackPlayer();
         }
     }
