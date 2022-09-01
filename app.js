ef77f2b5ca6ccc36b30e64829455f5ae394c0d90
Pasha-Akito <1masterpasha@gmail.com>
2022-09-01 10:06:04 +0100

    Added Special Attack button

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+//function out of scope of vue because it uses just plain javascript
 function getRandomValue(min, max) {
     return Math.floor(Math.random() * (max - min)) + min;
 }
@@ -6,19 +7,9 @@ const app = Vue.createApp({
     data() {
         return {
             playerHealth: 100,
-            monsterHealth: 100
+            monsterHealth: 100,
+            currentRound: 0
         };
-    },
-    methods: {
-        attackMonster() {
-            const attackValue = getRandomValue(5, 12);
-            this.monsterHealth -= attackValue;
-            this.attackPlayer();
-        },
-        attackPlayer() {
-            const attackValue = getRandomValue(8, 15);
-            this.playerHealth -= attackValue;
-        }
     },
     computed: {
         monsterBarStyles() {
@@ -28,6 +19,24 @@ const app = Vue.createApp({
             return {width: this.playerHealth + '%'}
         }
     },
+    methods: {
+        attackMonster() {
+            this.currentRound++;
+            const attackValue = getRandomValue(5, 12);
+            this.monsterHealth -= attackValue;
+            this.attackPlayer();
+        },
+        attackPlayer() {
+            const attackValue = getRandomValue(8, 15);
+            this.playerHealth -= attackValue;
+        },
+        specialAttackMonster() {
+            this.currentRound++;
+            const attackValue = getRandomValue(10, 25);
+            this.monsterHealth -= attackValue;
+            this.attackPlayer();
+        }
+    }
 });
 
 app.mount('#game');
